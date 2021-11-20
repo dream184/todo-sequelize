c19e171ac80286600b9ccfa604cbb8304d922228
shibao <dream0184@yahoo.com.tw>
2021-11-20 18:58:17 +0800

    feat: add user routes and views/login, views/register

--- app.js
+++ app.js
@@ -6,8 +6,31 @@ const port = 3000
 app.engine('hbs', exphbs.engine({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', '.hbs')
 
+app.use(express.static('public'))
+
+
+app.get('/users/login', (req, res) => {
+  res.render('login')
+})
+
+app.post('/users/login', (req, res) => {
+  res.send('login')
+})
+
+app.get('/users/register', (req, res) => {
+  res.render('register')
+})
+
+app.post('/users/register', (req, res) => {
+  res.send('register')
+})
+
+app.get('/users/logout', (req, res) => {
+  res.send('logout')
+})
+
 app.get('/', (req, res) => {
-  res.render('index')
+  res.render('login')
 })
 
 app.listen(port, () => {
